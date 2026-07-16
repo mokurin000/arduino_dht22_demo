d917f4efdbb57b3ba22ec84cd14cecd67f92b7ae
莯凛 <mokurin000@gmail.com>
2026-07-16 11:17:28 +0800

    feat: merge partitions into .uf2

---
+++ _wokwi_merge.sh
@@ -0,0 +1,29 @@
+if [ -d "./.pio/build/wokwi" ]; then
+    (
+        cd .pio/build/wokwi
+        esptool --chip esp32 \
+            merge-bin \
+            -o firmware.uf2 \
+            --flash-mode qio \
+            --format uf2 \
+            0x0 bootloader.bin \
+            0x8000 partitions.bin \
+            0x10000 firmware.bin
+        mv firmware.uf2 ../../../wokwi/esp32/
+    )
+fi
+
+if [ -d "./.pio/build/wokwi-c3-mini" ]; then
+    (
+        cd .pio/build/wokwi-c3-mini
+        esptool --chip esp32c3 \
+            merge-bin \
+            -o firmware.uf2 \
+            --flash-mode qio \
+            --format uf2 \
+            0x0 bootloader.bin \
+            0x8000 partitions.bin \
+            0x10000 firmware.bin
+        mv firmware.uf2 ../../../wokwi/esp32-c3/
+    )
+fi
